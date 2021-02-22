ccc83876c2a6201719f3bcd7cc96b39099bd3a9c
Kerry Lang <klang812@gmail.com>
2021-02-22 14:45:50 -0800

    add spec for multi-word capitals good fail
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -6,4 +6,8 @@ describe('#title_case') do
   it("takes a word and capitalizes first letter") do
     expect(title_case("bananas")).to(eq("Bananas"))
   end
+
+  it("capitalizes the first letter of each word in a multi-word string") do
+    expect(title_case("we're going bananas")).to(eq("We're Going Bananas"))
+  end
 end
